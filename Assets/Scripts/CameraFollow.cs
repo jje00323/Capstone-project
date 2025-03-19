70949c6c03a2b1dba1f03ae6b2fdc33a774d5ac1
jee00323 <jee00323@naver.com>
2025-03-19 15:02:58 +0900

    UI 메인메뉴 구현

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target; // 따라갈 대상 (플레이어)
-    public Vector3 offset = new Vector3(0, 10, -10); // 카메라 위치 오프셋
+    public Vector3 offset = new Vector3(0, 7, -4.5f); // 카메라 위치 오프셋
 
     void LateUpdate()
     {
